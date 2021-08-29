3efee615ef4565135d42c6b4624994b9f926d7f5
SushantS Kadam <SushantS.Kadam@ltisolvathon.onmicrosoft.com>
2021-08-29 23:33:36 +0530

    resume form auto fill

--- resume parser/application.py
+++ resume parser/application.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask import jsonify
 import json
 import requests
@@ -13,64 +13,48 @@ from werkzeug.utils import secure_filename
 UPLOAD_FOLDER = 'uploads'
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
-application = Flask(__name__)
+application = Flask(__name__,template_folder='template')
 application.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
+application.config['SESSION_TYPE'] = 'filesystem'
+application.secret_key = "f3cfe9ed8fae309f02079dbf"
 # print a nice greeting.
-def say_hello(username = "World"):
-    return '<p>Hello %s!</p>\n' % username
-
-# some bits of text for the page.
-header_text = '''
-    <html>\n<head> <title>EB Flask Test</title> </head>\n<body>'''
-instructions = '''
-        <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form action='/parse' method=post enctype=multipart/form-data>
-      <input type=file name=file>
-      <input type=submit value=Upload>
-    </form>
-    '''
-home_link = '<p><a href="/">Back</a></p>\n'
-footer_text = '</body>\n</html>'
-
-# EB looks for an 'application' callable by default.
-#application = Flask(__name__)
-#CORS(application)
-
-# add a rule for the index page.
-application.add_url_rule('/', 'index', (lambda: header_text +
-    say_hello() + instructions + footer_text))
-
-# add a rule when the page is accessed with a name appended to the site
-# URL.
-application.add_url_rule('/<username>', 'hello', (lambda username:
-    header_text + say_hello(username) + home_link + footer_text))
+@application.route("/")
+def hello():
+    return render_template('index.html')
 
 
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@application.route('/parse', methods=['POST'])
+@application.route('/parse_resume', methods=['GET','POST'])
 def parse_resume():
     #print(request.form)
-    if 'file' not in request.files:
-        flash('No file part')
-        return redirect(request.url)
-    file = request.files['file']
-    # if user does not select file, browser also
-    # submit an empty part without filename
-    if file.filename == '':
-        flash('No selected file')
-        return redirect(request.url)
-    if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(application.config['UPLOAD_FOLDER'], filename))
-        os.rename('uploads/'+file.filename,'uploads/resume.pdf')
-        data = ResumeParser('uploads/resume.pdf').get_extracted_data()
-        return data
+    print("hello", request.method)
+    
+    if request.method == 'POST':
+        print("IN POST")
+        
+        if 'file' not in request.files:
+            flash('No file part')
+            print('NO')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        print(file)
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(application.config['UPLOAD_FOLDER'], filename))
+            os.rename('uploads/'+file.filename,'uploads/resume.pdf')
+            data = ResumeParser('uploads/resume.pdf').get_extracted_data()
+            os.remove('uploads/resume.pdf')
+            print(data["email"])
+            return render_template('index.html', data=data)
+    return 'NO'#render_template('index.html')
         
 
     
